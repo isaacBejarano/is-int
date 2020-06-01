1531a7991dd9c334ab4b1d89e51b53a7be2610c5
Isaac Bejarano <44377797+isaacBejarano@users.noreply.github.com>
2020-06-01 02:58:50 +0200

    Update is-int.js

--- is-int.js
+++ is-int.js
@@ -5,10 +5,5 @@ function isInt(num) {
 	return num[num.length - 1] === "0" ? true : false;
 }
 
-/* NOTE
-    When multiplied by 10, last digit in 'integer' is always 0.
-    Javascript coherces ("" + 'number') to "string", which is iterable.
-    It also coherces 'bolean' and 'truthy' values to integer.  */
-
 // TEST
 // module.exports = { isInt }; // uncomment to enable JEST
